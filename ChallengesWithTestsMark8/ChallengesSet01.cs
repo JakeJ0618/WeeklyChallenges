c2a0d4353bf9f55e0675c7df4353721f19f94b0e
Jake Johnston <jakejohnston618@gmail.com>
2023-03-16 09:49:52 -0500

    finished challenges

--- ChallengesWithTestsMark8/ChallengesSet01.cs
+++ ChallengesWithTestsMark8/ChallengesSet01.cs
@@ -1,10 +1,11 @@
 ﻿using System;
+using System.ComponentModel;
 
 namespace ChallengesWithTestsMark8
 {
     public class ChallengesSet01
     {
-        public bool AreTwoNumbersTheSame(int num1, int num2)
+        public static bool AreTwoNumbersTheSame(int num1, int num2)
         {
             if(num1 == num2)
             {
@@ -16,34 +17,75 @@ namespace ChallengesWithTestsMark8
             }
         }
 
-        public double Subtract(double minuend, double subtrahend)
+        public static double Subtract(double minuend, double subtrahend)
         {
-            throw new NotImplementedException();
+            Console.WriteLine("Please choose your first number.");
+            minuend = double.Parse(Console.ReadLine());
+            Console.WriteLine("Please choose your second number.");
+            subtrahend = double.Parse(Console.ReadLine());
+            return minuend - subtrahend;
         }
 
-        public int Add(int number1, int number2)
+        public static int Add(int number1, int number2)
         {
-            throw new NotImplementedException();
+            Console.WriteLine("Please choose your first number.");
+            number1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please choose your second number");
+            number2 = int.Parse(Console.ReadLine());
+            return (number1 + number2);
+        }
+        public static void GetSmallestNumber(int number1, int number2)
+        {
+            Console.WriteLine("Please choose your first number.");
+            number1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please choose your second number.");
+            number2 = int.Parse(Console.ReadLine());
+            
+            
+            
+            if(number1 < number2)
+            {
+                Console.WriteLine("Your first number is smaller than the second one.");
+            }
+            else
+            {
+                Console.WriteLine("Your second number is smaller than the first one.");
+            }
+            return ;
+            
         }
 
-        public int GetSmallestNumber(int number1, int number2)
+        public static long Multiply(long factor1, long factor2)
         {
-            throw new NotImplementedException();
+            Console.WriteLine("Please choose your first number.");
+            factor1 = long.Parse(Console.ReadLine());
+            Console.WriteLine("Please choose your second number.");
+            factor2 = long.Parse(Console.ReadLine());
+            return factor1 * factor2;
+            
         }
 
-        public long Multiply(long factor1, long factor2)
+        public static string GetGreeting(string nameOfPerson)
         {
-            throw new NotImplementedException();
-        }
-
-        public string GetGreeting(string nameOfPerson)
-        {
-            throw new NotImplementedException();
+            Console.WriteLine("Hello, what's your name?");
+            var fullName = Console.ReadLine();
+            Console.WriteLine($"Hello, {fullName}");
+            return nameOfPerson;
         }
 
         public string GetHey()
         {
-            throw new NotImplementedException();
+            Console.WriteLine("Hey!");
+            return GetHey();
         }
+            
+            
+        
+
+
+       
+        
     }
-}
+    
+    
+} 
